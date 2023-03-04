41f3b6c52d90fbfd6c6c81a1f4352dc439ecc3c9
kkgonsovsky <kgonsovsky@tennisi.it>
2023-03-04 07:55:03 +0300

    Q

--- push.bat
+++ push.bat
@@ -1,4 +1,4 @@
 ﻿git add .
 git commit -m "Q"
 echo %GITHUB_TOKEN%
-git push https://kkgonsovsky:%GITHUB_TOKEN%@github.com/kkgonsovsky/StrongTypedIdPackage.git
+git push https://kkgonsovsky:%GITHUB_TOKEN%@github.com/kkgonsovsky/StrongTypedIdPackage.git --force
