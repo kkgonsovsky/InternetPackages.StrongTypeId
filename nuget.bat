40ca7433dd5f32e54b8ea89bd342056b207d7771
kkgonsovsky <kgonsovsky@tennisi.it>
2023-03-04 10:09:57 +0300

    Q

--- nuget.bat
+++ nuget.bat
@@ -3,5 +3,5 @@ set version=1.1.6
 dotnet build --configuration Release /p:Version=%version%
 dotnet pack --configuration Release /p:Version=%version%
 
-dotnet nuget push "src\bin\Release\InternetPackages.StrongTypeId.%version%.nupkg" --source "https://api.nuget.org/v3/index.json" --api-key "oy2h54hkeuippt5orwyakhopqpagwd3edcsanw3aedshxi"
+
 
